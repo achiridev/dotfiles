d3166d99f75c48316e277f35ebd2318c7518c12c
achiridev <cooperbrawl@gmail.com>
2026-05-14 18:27:12 -0500

    feat: migrate waybar config

---
+++ config/waybar/.config/waybar/scripts/power.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+wlogout \
+    --protocol layer-shell \
+    --buttons-per-row 3 \
+    --column-spacing 20 \
+    --row-spacing 20 \
+    --margin-top 150 \
+    --margin-bottom 150 \
+    --margin-left 500 \
+    --margin-right 500
