f3245bf9cbb7faff10020a9ca50856248270434c
achiridev <cooperbrawl@gmail.com>
2026-05-14 18:26:31 -0500

    feat(config): add full desktop environment configs (GTK, Qt, nvim, rofi, fastfetch)

---
+++ config/nvim/.config/nvim/init.lua
@@ -0,0 +1,33 @@
+print("My own neovim")
+
+print("Loading lazy")
+require("config.lazy")
+
+print('QLS')
+
+vim.o.shiftwidth = 3
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.cursorline = true
+vim.o.scrolloff = 10
+
+-- Sync clipboard between OS and Neovim. Schedule the setting after `UiEnter` because it can
+-- increase startup-time. Remove this option if you want your OS clipboard to remain independent.
+-- See `:help 'clipboard'`
+vim.api.nvim_create_autocmd('UIEnter', {
+  callback = function()
+    vim.o.clipboard = 'unnamedplus'
+  end,
+})
+
+-- [[ Basic Autocommands ]].
+-- See `:h lua-guide-autocommands`, `:h autocmd`, `:h nvim_create_autocmd()`
+
+-- Highlight when yanking (copying) text.
+-- Try it with `yap` in normal mode. See `:h vim.hl.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  callback = function()
+    vim.hl.on_yank()
+  end,
+})
